8e15d58c48234bf26048a59f0228ade391712700
YegorChevardin <egor03052004@gmail.com>
2023-04-01 21:32:41 +0200

    Implement set up for repository subproject

---
+++ web/src/main/java/ua/com/epam/lab/yegorchevardin/spring/certificatesystem/Main.java
@@ -0,0 +1,7 @@
+package ua.com.epam.lab.yegorchevardin.spring.certificatesystem;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
